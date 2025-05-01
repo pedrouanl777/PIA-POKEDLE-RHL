3de86b325c0e6705d243d677c737615de147e87d
Huntdown05 <57652486+jlpz05@users.noreply.github.com>
2025-05-01 21:43:42 +0000

    commit

--- Experimento inicial.py
+++ Experimento inicial.py
@@ -1,5 +1,5 @@
 import requests
-import random
+import random #para hacer mas facil el que agarre un numero aleatorio
 url = "https://pokeapi.co/api/v2/pokemon?limit=151"  # Lista de Pokémon, no uno solo
 #Nota, modifuque el 20 por 151 para que tomara los priemeros 151 pokemons (primera generacion)
 
@@ -7,9 +7,9 @@ response = requests.get(url)
 data = response.json()
 
 # Verifica qué hay dentro del JSON
-print(data.keys())  # Esto debe mostrar: dict_keys(['count', 'next', 'previous', 'results'])
-x = random.randrange(1, 151)
-# Ahora accede al primer Pokémon
+print(data.keys())  # Esto debe mostrar: dict_keys(['count', 'next', 'previous', 'results'])  #este se puede quitar sin problemas
+x = random.randrange(1, 151) #En un rango de 1 a 151 elije un numero y lo guarda en X
+# Ahora accede al Pokémon
 pokemon = data["results"][x]["name"]
 
 print(pokemon)
