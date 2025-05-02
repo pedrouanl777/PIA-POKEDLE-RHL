e5e3009f95fc009aefcf5deaf2b1bcc229fe1413
Huntdown05 <57652486+jlpz05@users.noreply.github.com>
2025-05-02 02:35:11 +0000

    commit

--- Intentos de filtrar informacion/Experimento inicial 4.py
+++ Intentos de filtrar informacion/Experimento inicial 4.py
@@ -3,7 +3,7 @@ import json
 base_url = "https://pokeapi.co/api/v2/"
 x=0
 pokemon = {}
-while x < 151:
+while x < 2:
     def get_pokemon_info(id): #Una funcion para usar después 
         url = f"{base_url}/pokemon/{id}" #Esto para que podamos cambiar la url a lo que necesitamos, una url que puede cambiar la id
         response = requests.get(url)
@@ -19,8 +19,8 @@ while x < 151:
     y = f"{pokemon_info["name"].capitalize()}"
     if pokemon_info: #Ando pensando en quitar este if, pero no estoy seguro 
         Tipos = [typ["type"]["name"] for typ in pokemon_info["types"]] #este tuvo que ser diferente, porque al haber 2 instancias de "Types" soltaba error
-        pokemon.update({y: "id: " f"{pokemon_info["id"]}, " "altura: " f"{pokemon_info["height"]}" "m, " "Peso: " f"{pokemon_info["weight"]}" "Kg, " "Pokemon de tipo: "})
+        pokemon.update({y:  "id: " f"{pokemon_info["id"]}, " "altura: " f"{pokemon_info["height"]}" "m, " "Peso: " f"{pokemon_info["weight"]}" "Kg, " "Pokemon de tipo: "  })
 json_string = json.dumps(pokemon, indent=4)
 print(json_string)
-with open("data.json", "w") as f:
+with open("datos estructurados.json", "w") as f:
     json.dump(pokemon, f, indent=4)
