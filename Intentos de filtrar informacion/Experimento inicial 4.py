1f419886f8464000fbbf14c99fdea5b831f86529
Huntdown05 <57652486+jlpz05@users.noreply.github.com>
2025-05-02 00:17:59 +0000

    commit

--- Intentos de filtrar informacion/Experimento inicial 4.py
+++ Intentos de filtrar informacion/Experimento inicial 4.py
@@ -2,7 +2,8 @@ import requests
 import random #importar random para no solo limitar la cantidad de pokemon, si no tambien que se elija un pokemon aleatorio cada que se active el modulo
 base_url = "https://pokeapi.co/api/v2/"
 x=0
-while x < 151:
+pokemon = {}
+while x < 2:
     def get_pokemon_info(id): #Una funcion para usar después 
         url = f"{base_url}/pokemon/{id}" #Esto para que podamos cambiar la url a lo que necesitamos, una url que puede cambiar la id
         response = requests.get(url)
@@ -15,11 +16,8 @@ while x < 151:
     x = x+1 #elige un numero del 1 al 151
     pokemon_id = x #lo graba en la id
     pokemon_info = get_pokemon_info(pokemon_id) #saca la informacion de la id
-
+    y = f"Nombre: {pokemon_info["name"].capitalize()}"
     if pokemon_info: #Ando pensando en quitar este if, pero no estoy seguro 
-        print(f"Nombre: {pokemon_info["name"].capitalize()}") #Como todos los nombres estan en minuscula en el Json, le ponemos mayusculas al inicio
-        print(f"Id: {pokemon_info["id"]}")
-        print(f"Altura: {pokemon_info["height"]}", "m")
-        print(f"Peso: {pokemon_info["weight"]}", "Kg")
         Tipos = [typ["type"]["name"] for typ in pokemon_info["types"]] #este tuvo que ser diferente, porque al haber 2 instancias de "Types" soltaba error
-        print ("El Pokemon es de tipo:", Tipos) 
+        pokemon.update({y: "id: " f"{pokemon_info["id"]}, " "altura: " f"{pokemon_info["height"]}" "m, " "Peso: " f"{pokemon_info["weight"]}" "Kg, " "Pokemon de tipo: "})
+print (pokemon)
