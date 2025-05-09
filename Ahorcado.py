8738a6c5ce03b4e7c7886ae7dbed5558802b0c02
Huntdown05 <57652486+jlpz05@users.noreply.github.com>
2025-05-09 02:09:40 +0000

    Al fin puedo sacar la id con el json

--- Ahorcado.py
+++ Ahorcado.py
@@ -1,12 +1,13 @@
 import json
 import numpy as np 
 import statistics
-
+print("Ingrese su nickname, este sera guardado para recoleccion de datos y lo podra seguir usando para futuro")
+#nombre = input(str("Nickname: "))
 archivo = open("datos_estructurados.json", "r")
 datos = json.load(archivo)
 num = str(np.random.randint(1,151))
-Palabra = datos[num]*len
-print("""Bienvenidos al prototipo del Pokedle
+Palabra = datos[num]
+print("""                       Bienvenidos al prototipo del Pokedle
             Aqui estan los datos del pokemon elegido por nuestro eficiente codigo""") #Por ahora funcionara como un ahorcado
 print("La entrada de la pokedex es: ", num)
 print("El peso es: ", datos[num]["peso"])
